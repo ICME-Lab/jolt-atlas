1572937de591a3b6d33a07ecd2de2409d45ff9e9
acentelles <centelles.alberto@gmail.com>
2025-08-29 15:16:53 +0100

    Pass tests

--- zkml-jolt-core/src/jolt/instruction/sigmoid.rs
+++ zkml-jolt-core/src/jolt/instruction/sigmoid.rs
@@ -1,6 +1,4 @@
-use crate::jolt::execution_trace::ONNXLookupQuery;
 use jolt_core::jolt::{instruction::{InstructionLookup, LookupQuery}, lookup_table::{sigmoid::{SCALE, SigmoidTable}, LookupTables}};
-use onnx_tracer::constants::MAX_TENSOR_SIZE;
 use serde::{Deserialize, Serialize};
 
 
@@ -31,7 +29,7 @@ impl<const WORD_SIZE: usize> LookupQuery<WORD_SIZE> for SigmoidInstruction<WORD_
     }
 
     fn to_lookup_index(&self) -> u64 {
-        LookupQuery::<WORD_SIZE>::to_lookup_operands(self).1
+        LookupQuery::<WORD_SIZE>::to_lookup_operands(self).0
     }
 
 
@@ -44,8 +42,7 @@ impl<const WORD_SIZE: usize> LookupQuery<WORD_SIZE> for SigmoidInstruction<WORD_
 #[cfg(test)]
 mod test {
     use crate::jolt::execution_trace::JoltONNXCycle;
-
-    use super::*;
+    use crate::jolt::execution_trace::ONNXLookupQuery;
     use jolt_core::jolt::instruction::InstructionLookup;
     use onnx_tracer::trace_types::{ONNXCycle, ONNXOpcode};
     use rand::{rngs::StdRng, SeedableRng};
@@ -55,21 +52,14 @@ mod test {
         let mut rng = StdRng::seed_from_u64(12345);
         let cycle: ONNXCycle = ONNXCycle::random(ONNXOpcode::Sigmoid, &mut rng);
         let jolt_cycle = JoltONNXCycle::from_raw(&cycle);
-        println!("jolt_cycle: {:?}", jolt_cycle);
         let table = jolt_cycle.lookup_table().unwrap();
-        for _ in 0..1 {
-            // let random_cycle = jolt_cycle.random(&mut rng);
+        for _ in 0..1000 {
             let output = <JoltONNXCycle as ONNXLookupQuery<32>>::to_lookup_output(&jolt_cycle);
-            println!("output: {:?}", output[0]);
             let index = <JoltONNXCycle as ONNXLookupQuery<32>>::to_lookup_index(&jolt_cycle);
-            println!("index: {:?}", index[0]);
             let materialized_entry = table.materialize_entry(index[0]);
-            println!("materialized_entry: {:?}", materialized_entry);
             assert_eq!(
                 output[0],
                 materialized_entry,
-                "{:?}",
-                jolt_cycle.memory_ops
             );
         }
     }

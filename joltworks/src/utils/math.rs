adba47fe583853cdfbeb99d2b204e07f75fd31b6
clankpan <clankpan26cm@gmail.com>
2026-03-05 17:02:10 +0900

    fix(math): make log_2 panic on non-power-of-two inputs

--- joltworks/src/utils/math.rs
+++ joltworks/src/utils/math.rs
@@ -14,12 +14,11 @@ impl Math for usize {
 
     fn log_2(self) -> usize {
         assert_ne!(self, 0);
-
-        if self.is_power_of_two() {
-            (1usize.leading_zeros() - self.leading_zeros()) as usize
-        } else {
-            (0usize.leading_zeros() - self.leading_zeros()) as usize
-        }
+        assert!(
+            self.is_power_of_two(),
+            "log_2 is only defined for power-of-two values, got {self}"
+        );
+        (1usize.leading_zeros() - self.leading_zeros()) as usize
     }
 }
 

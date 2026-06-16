46fda37bc53b05393aa82d591c963c48b0138539
AntoineF4C5 <antoinedcht@hotmail.com>
2026-06-16 13:31:38 +0200

    feat: single bounded clamping

--- joltworks/src/lookup_tables/suffixes/less_than.rs
+++ joltworks/src/lookup_tables/suffixes/less_than.rs
@@ -4,9 +4,9 @@ use super::SparseDenseSuffix;
 
 /// 1 if the first operand is (strictly) less than the
 /// second operand; 0 otherwise.
-pub enum LessThanSuffix {}
+pub enum LessThanSuffix<const XLEN: usize> {}
 
-impl SparseDenseSuffix for LessThanSuffix {
+impl<const XLEN: usize> SparseDenseSuffix for LessThanSuffix<XLEN> {
     fn suffix_mle(b: LookupBits) -> u32 {
         let (x, y) = b.uninterleave();
         (u32::from(x) < u32::from(y)).into()

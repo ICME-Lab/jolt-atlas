1ecf5010869ea44c46a1216cea004f465d04cc16
acentelles <centelles.alberto@gmail.com>
2026-09-17 16:50:31 -0400

    Prove signed clamping before private table lookups

--- joltworks/src/poly/commitment/dory/mod.rs
+++ joltworks/src/poly/commitment/dory/mod.rs
@@ -31,6 +31,8 @@ pub mod native_boundary;
 #[cfg(feature = "zk")]
 mod native_add;
 #[cfg(feature = "zk")]
+mod native_clamped_lookup;
+#[cfg(feature = "zk")]
 mod native_einsum;
 #[cfg(feature = "zk")]
 pub mod native_graph;

02163ea6302999fc84fb49468c4a064ad3f4466d
acentelles <centelles.alberto@gmail.com>
2026-09-21 16:10:29 +0100

    Reject uncommitted graph witnesses at external tensor boundaries

--- joltworks/src/poly/commitment/dory/native_boundary.rs
+++ joltworks/src/poly/commitment/dory/native_boundary.rs
@@ -229,7 +229,7 @@ mod tests {
         }
     }
     #[test]
-    fn boundary_binds_prior_commitment_and_checks_context() {
+    fn boundary_binds_prior_commitment_and_rejects_uncommitted_witness() {
         let pp = DoryScheme::setup_prover(12);
         let vp = DoryScheme::setup_verifier(&pp);
         let graph = NativeGraph {
@@ -238,6 +238,7 @@ mod tests {
             nodes: vec![NativeGraphNode::lookup(0, vec![4, 5], 1)],
             outputs: vec![1],
         };
+        let loose = NativeGraphWitness::uncommitted(&graph, vec![vec![0, 1, 0, 1]]).unwrap();
         let (statement, witness) =
             NativeGraphWitness::commit(graph, vec![vec![0, 1, 0, 1]], &pp).unwrap();
         let a =
@@ -247,6 +248,9 @@ mod tests {
             NativeTensorBoundaryProof::evaluation_point(&statement, b"boundary", 1, [2; 32], &vp)
                 .unwrap();
         assert_ne!(a, b);
+        assert!(loose
+            .prove_tensor_boundary(&statement, b"boundary", 1, [1; 32], &pp)
+            .is_err());
         assert!(witness
             .prove_tensor_boundary(&statement, b"", 1, [1; 32], &pp)
             .is_err());
